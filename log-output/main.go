4b97ac4261701d1d3b1142cfeac961f3b25ef1ae
trong.tek <trong.vu@sotatek.com>
2022-08-04 11:09:18 +0700

    Exercise 1.07: External access with Ingress

--- log-output/main.go
+++ log-output/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
 func main() {
 
 	randomStr, err := uuid.NewUUID()
+	var timeStr string
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	for {
-		fmt.Printf("%s: %s\n", time.Now().Format("2006-01-02T15:04:05Z07:00"), randomStr)
-		time.Sleep(time.Second * 5)
-	}
+	go func() {
+		for {
+			timeStr = time.Now().Format("2006-01-02T15:04:05Z07:00")
+			fmt.Printf("%s: %s\n", timeStr, randomStr)
+			time.Sleep(time.Second * 5)
+		}
+	}()
+
+	app := gin.Default()
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"string": randomStr,
+			"time":   timeStr,
+		})
+	})
+
+	run(app, "8080")
+
+}
+
+func run(app *gin.Engine, port string) error {
+	log.Println("Server started on port", port)
+	return http.ListenAndServe(fmt.Sprintf(":%s", port), app.Handler())
 }
