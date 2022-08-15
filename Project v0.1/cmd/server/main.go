4346f68a284a8ef633defb3c844a915fe8e5fb66
trong.tek <trong.vu@sotatek.com>
2022-08-15 16:50:30 +0700

    Exercise 1.12: Project v0.6

--- Project v0.1/cmd/server/main.go
+++ Project v0.1/cmd/server/main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/hiiamtrong/todo-simple/config"
 )
 
 func main() {
+
+	go func() {
+		if !fileExists("./public/thumbnail.png") {
+			err := saveImageFromUrl("https://picsum.photos/1200")
+
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println("Create thumbnail.png")
+			}
+
+		} else {
+			for {
+
+				if time.Now().Hour() == 0 && time.Now().Minute() == 0 {
+					err := saveImageFromUrl("https://picsum.photos/1200")
+					if err != nil {
+						log.Println(err)
+					} else {
+						log.Println("Update thumbnail.png")
+					}
+				}
+			}
+		}
+
+	}()
 
 	gin.SetMode(gin.ReleaseMode)
 
@@ -18,6 +47,7 @@ func main() {
 	app := gin.Default()
 
 	app.LoadHTMLGlob("templates/*")
+	app.Static("/public", "./public")
 
 	app.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
@@ -29,4 +59,45 @@ func main() {
 func run(app *gin.Engine) error {
 	log.Println("Server started on port", config.Cfg.Port)
 	return http.ListenAndServe(fmt.Sprintf(":%s", config.Cfg.Port), app.Handler())
+}
+
+func saveImageFromUrl(url string) error {
+	// TODO
+	response, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	file, err := os.OpenFile("./public/thumbnail.png", os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(data)
+
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+// check if file exists
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
