b703ad9dce28463db9976a1aa7a71f783a898a10
trong.tek <trong.vu@sotatek.com>
2022-08-16 18:01:27 +0700

    Exercise 2.02: Project v1.0

---
+++ Project v0.1/cmd/client/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hiiamtrong/todo-simple/config"
+)
+
+func main() {
+
+	go func() {
+		if !fileExists("./public/thumbnail.png") {
+			err := saveImageFromUrl("https://picsum.photos/1200")
+
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println("Create thumbnail.png")
+			}
+
+		} else {
+			for {
+
+				if time.Now().Hour() == 0 && time.Now().Minute() == 0 {
+					err := saveImageFromUrl("https://picsum.photos/1200")
+					if err != nil {
+						log.Println(err)
+					} else {
+						log.Println("Update thumbnail.png")
+					}
+				}
+			}
+		}
+
+	}()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	config.Init()
+
+	app := gin.Default()
+
+	app.LoadHTMLGlob("templates/*")
+	app.Static("/public", "./public")
+
+	app.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	log.Fatal(run(app))
+}
+
+func run(app *gin.Engine) error {
+	log.Println("Server started on port", config.Cfg.PortClient)
+	return http.ListenAndServe(fmt.Sprintf(":%s", config.Cfg.PortClient), app.Handler())
+}
+
+func saveImageFromUrl(url string) error {
+	// TODO
+	response, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	file, err := os.OpenFile("./public/thumbnail.png", os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(data)
+
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+// check if file exists
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
--- Project v0.1/config/config.go
+++ Project v0.1/config/config.go
@@ -3,13 +3,15 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	Port string `mapstructure:"port"`
+	PortClient string `mapstructure:"port_client"`
+	PortServer string `mapstructure:"port_server"`
 }
 
 var Cfg Config
 
 func Init() {
-	viper.SetDefault("port", "8080")
+	viper.SetDefault("port_client", "8080")
+	viper.SetDefault("port_server", "9090")
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
